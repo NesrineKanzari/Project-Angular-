9096f6d6fa6989aa77c16d1009c184e17d3146b4
NesrineKanzari <nesrinekan17@gmail.com>
2021-01-31 00:29:24 +0100

    first commit

---
+++ Projet_Nesrine/src/app/formateurs/formateurs.ts
@@ -0,0 +1,20 @@
+import { Formateur } from "./formateur";
+
+export const FORMATEURSLIST: Formateur[] = [
+    {
+        id: 1,
+        name: 'Kanzari',
+        firstname: 'Nesrine',
+        adress: 'Tunis',
+        email: 'kanzarinesrine@gmail.com',
+        expertise: 'Angular'
+    },
+    {
+        id: 2,
+        name: 'test',
+        firstname: 'test',
+        adress: 'Ariana',
+        email: 'testtest@gmail.com',
+        expertise: 'Mobile'
+    }
+]
---
+++ Projet_Nesrine/src/app/participants/participants.ts
@@ -0,0 +1,24 @@
+import { Participant } from "./participant";
+
+export const PARTICIPANTSLIST: Participant[] = [
+    {
+        id: 1,
+        cin : '14765442',
+        name: 'Kanzari',
+        firstname: 'Nessrine',
+        adress: 'Tunis',
+        email: 'kanzarinesrine@gmail.com',
+        societe: 'ENICarthage',
+        poste: "developer"
+    },
+    {
+        id: 2,
+        cin : '07472132',
+        name: 'test',
+        firstname: 'test',
+        adress: 'Ariana',
+        email: 'testtest@gmail.com',
+        societe: 'ENICarthage',
+        poste: "developer"
+    }
+]
